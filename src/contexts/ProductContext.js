741328fb73b7f00894db035d32019ece9bd58918
yash131120 <139432375+yash131120@users.noreply.github.com>
2023-12-11 15:32:28 +0530

    E-Commerce_Woman_JS

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+// create context
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  // products state
+  const [product, setProducts] = useState([]);
+  // fetch product
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return <ProductContext.Provider value={{ product }}>{children}</ProductContext.Provider>;
+};
+
+export default ProductProvider;
---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+//import product context
+import { ProductContext } from '../contexts/ProductContext';
+// import components
+import Product from '../components/Product';
+import Hero from '../components/Hero';
+
+const Home = () => {
+  //get products from product context
+  const { product } = useContext(ProductContext);
+  // get only men's & women's clothing category
+  const filteredProducts = product.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+
+  return (
+    <div>
+      <Hero />
+      <section className='py-16'>
+        <div className='container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+            {filteredProducts.map((product) => {
+              return (
+                <Product product={product} key={product.id} />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+
+};
+
+export default Home;
+
+
+
+
+
+//19:30
